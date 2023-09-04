4041959ef4826d5c33c13f0f330fbb4576edcf62
Octavio Enrique Morales García <octavius@cesmar.edu.ni>
2023-09-04 10:37:56 -0600

    Dark mode available
    Enabled dark mode in all pages

--- assets/js/themeChange.js
+++ assets/js/themeChange.js
@@ -1,15 +1,25 @@
+let themeActive = 'dark';
+document.documentElement.setAttribute('data-bs-theme', themeActive);
+
+document.getElementById('btnTheme').setAttribute('class', 'btn btn-outline-light me-2');
+document.getElementById('themeIcon').setAttribute('class', 'bi bi-moon-fill');
+
+
 //Asignar el evento click al boton btnTheme
 document.getElementById('btnTheme').addEventListener("click", changeTheme);
+
 
 //Funcion para cambiar el tema
 function changeTheme(){
     // Si el data-bs-theme en la etiqueta html es light, cambiar a dark
     if(document.documentElement.getAttribute('data-bs-theme') === 'light'){
-        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        themeActive = 'dark';
+        document.documentElement.setAttribute('data-bs-theme', themeActive);
         document.getElementById('btnTheme').setAttribute('class', 'btn btn-outline-light me-2');
         document.getElementById('themeIcon').setAttribute('class', 'bi bi-moon-fill');
     }else{
-        document.documentElement.setAttribute('data-bs-theme', 'light');
+        themeActive = 'light';
+        document.documentElement.setAttribute('data-bs-theme', themeActive);
         document.getElementById('btnTheme').setAttribute('class', 'btn btn-outline-dark me-2');
         document.getElementById('themeIcon').setAttribute('class', 'bi bi-sun-fill');
     }
