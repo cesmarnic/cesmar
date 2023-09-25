7d146c3ff1e5cc5eddc75591b6527d8b03709717
Octavio Enrique Morales García <octavius@cesmar.edu.ni>
2023-09-25 11:38:56 -0600

    Theme in local storage
    The theme value gonna save in the local storage from the explorer

--- assets/js/themeChange.js
+++ assets/js/themeChange.js
@@ -1,13 +1,11 @@
-let themeActive = 'dark';
+let themeActive = localStorage.getItem('theme') || 'dark';
 document.documentElement.setAttribute('data-bs-theme', themeActive);
 
-document.getElementById('btnTheme').setAttribute('class', 'btn btn-outline-light me-2');
-document.getElementById('themeIcon').setAttribute('class', 'bi bi-moon-fill');
-
+document.getElementById('btnTheme').setAttribute('class', themeActive === 'light' ? 'btn btn-outline-dark me-2' : 'btn btn-outline-light me-2');
+document.getElementById('themeIcon').setAttribute('class', themeActive === 'light' ? 'bi bi-sun-fill' : 'bi bi-moon-fill');
 
 //Asignar el evento click al boton btnTheme
 document.getElementById('btnTheme').addEventListener("click", changeTheme);
-
 
 //Funcion para cambiar el tema
 function changeTheme(){
@@ -23,4 +21,6 @@ function changeTheme(){
         document.getElementById('btnTheme').setAttribute('class', 'btn btn-outline-dark me-2');
         document.getElementById('themeIcon').setAttribute('class', 'bi bi-sun-fill');
     }
+    // Almacenar el valor del tema en el Local Storage
+    localStorage.setItem('theme', themeActive);
 }
